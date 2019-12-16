79cd3b3e0a25dca94d5fd8d3a957a8b71d3bd434
l1va <lev_matematik@tut.by>
2019-12-16 16:17:17 +0300

    debug false

--- robot/settings.py
+++ robot/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xti88a32*kde+50v)hpp&sa3kjtvp5gigxv3gl)1xxp4thm8v$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
